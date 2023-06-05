73a7c5a84850ce40cc77070c2cd1f07eb454107a
Emiliana Zapata Ruiz <111257963+emi0483@users.noreply.github.com>
2023-06-05 12:53:48 -0500

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -4,15 +4,16 @@
 #include <QGraphicsPixmapItem>
 #include <QObject>
 
-class Bullet : public QObject, public QGraphicsPixmapItem
+class Bullet : public QObject, public QGraphicsPixmapItem // Declaración de la clase "Bullet" que hereda públicamente de QObject y QGraphicsPixmapItem.
 {
     Q_OBJECT
 
 public:
-    Bullet(QGraphicsItem* parent=0);
+    Bullet(QGraphicsItem* parent=0); 
+//Declaración del constructor de la clase "Bullet". Toma un puntero a un objeto QGraphicsItem como argumento y tiene un valor predeterminado de 0 (puntero nulo).
 
 public slots:
-    void move();
+    void move(); //Declaración de la ranura "move()", que se llama cuando se produce una señal relacionada con el movimiento de la bala.
 };
 
 
