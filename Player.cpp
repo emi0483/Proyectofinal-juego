50ee38239f34dbf5438e289b955a621e0f2b37cb
Emiliana Zapata Ruiz <111257963+emi0483@users.noreply.github.com>
2023-06-05 21:24:31 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -32,6 +32,26 @@ void Player::keyPressEvent(QKeyEvent *event)
         scene()->addItem(bullet);
 
     }
+class Playerelipse : public QGraphicsEllipseItem
+{
+public:
+    Player() : QGraphicsEllipseItem(-10, -10, 20, 20) {
+        setBrush(Qt::red);
+    }
+
+    void keyPressEvent(QKeyEvent* event) override {
+        if (event->key() == Qt::Key_Right) {
+            setPos(x() + 10, y());
+        } else if (event->key() == Qt::Key_Left) {
+            setPos(x() - 10, y());
+        } else if (event->key() == Qt::Key_Up) {
+            setPos(x(), y() - 10);
+        } else if (event->key() == Qt::Key_Down) {
+            setPos(x(), y() + 10);
+        }
+    }
+};
+
 }
 void Player::spawn()
 {
